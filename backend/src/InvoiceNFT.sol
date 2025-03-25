bd25960cb21fac89a07324d9b88dbfb88e18798d
Aurélien Audelin <aurelien.audelin@gmail.com>
2025-03-25 10:29:45 +0100

    feat: add get Invoice

--- backend/src/InvoiceNFT.sol
+++ backend/src/InvoiceNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.29;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 /**
  * @title InvoiceNFT
  * @notice InvoiceNFT is a contract that allows users to create and manage invoices as NFTs
@@ -10,6 +10,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  */
 contract InvoiceNFT is ERC721, Ownable {
     uint256 private _tokenIdCounter;
+    mapping(uint256 => Invoice) private _invoices;
 
     /**
      * @notice CreditScore enum to store the credit score details
@@ -62,15 +63,31 @@ contract InvoiceNFT is ERC721, Ownable {
         OVERDUE
     }
 
-    mapping(uint256 => Invoice) private _invoices;
+    constructor(address owner) ERC721("Prima Invoice", "PIT") Ownable(owner) {}
 
-    constructor(address owner) ERC721("InvoiceNFT", "INV") Ownable(owner) {}
-
+    /**
+     * @notice Create a new Invoice as a NFT
+     * @dev Only the owner can create a new invoice
+     * @dev The invoice is minted to the Creditor
+     * @param to: address of the Creditor
+     * @param invoice: Invoice struct containing the invoice details
+     * @return tokenId: tokenId of the newly created invoice
+     */
     function createInvoice(address to, Invoice calldata invoice) external onlyOwner returns (uint256) {
         _tokenIdCounter++;
-        _mint(to, _tokenIdCounter);
-        _setApprovalForAll(to, owner(), true);
+        _safeMint(to, _tokenIdCounter);
         _invoices[_tokenIdCounter] = invoice;
         return _tokenIdCounter;
+    }
+
+    /**
+     * @notice Get the invoice details
+     * @dev Only the owner can get the invoice details
+     * @param tokenId: tokenId of the invoice
+     * @return invoice: Invoice struct containing the invoice details
+     */
+    function getInvoice(uint256 tokenId) external view onlyOwner returns (Invoice memory) {
+        _requireOwned(tokenId);
+        return _invoices[tokenId];
     }
 }
--- backend/test/InvoiceNFT.t.sol
+++ backend/test/InvoiceNFT.t.sol
@@ -3,40 +3,170 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {InvoiceNFT} from "../src/InvoiceNFT.sol";
+import {IERC721Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract InvoiceNFTTest is Test {
+contract InvoiceNFTCreateInvoiceTest is Test {
     InvoiceNFT private invoiceNFT;
     address private debtor = makeAddr("debtor");
     address private creditor = makeAddr("creditor");
     address private investor = makeAddr("investor");
     address private owner = address(this);
+    InvoiceNFT.Company private creditorCompany;
+    InvoiceNFT.Company private debtorCompany;
+    InvoiceNFT.Company private investorCompany;
+    InvoiceNFT.Invoice private invoice;
 
     function setUp() public {
         invoiceNFT = new InvoiceNFT(owner);
+        investorCompany = InvoiceNFT.Company(
+            investor,
+            address(invoiceNFT),
+            InvoiceNFT.CreditScore.C
+        );
+        debtorCompany = InvoiceNFT.Company(
+            debtor,
+            address(invoiceNFT),
+            InvoiceNFT.CreditScore.B
+        );
+        creditorCompany = InvoiceNFT.Company(
+            creditor,
+            address(invoiceNFT),
+            InvoiceNFT.CreditScore.A
+        );
+        invoice = InvoiceNFT.Invoice(
+            "1234567890",
+            "Activity",
+            "USA",
+            block.timestamp,
+            1000,
+            300,
+            creditorCompany,
+            debtorCompany,
+            investorCompany,
+            InvoiceNFT.InvoiceStatus.NEW
+        );
     }
 
-    function test_CreateInvoice() public {
-        InvoiceNFT.Company memory creditorCompany = InvoiceNFT.Company(creditor, address(invoiceNFT), InvoiceNFT.CreditScore.A);
-        InvoiceNFT.Company memory debtorCompany = InvoiceNFT.Company(debtor, address(invoiceNFT), InvoiceNFT.CreditScore.B);
-        InvoiceNFT.Company memory investorCompany = InvoiceNFT.Company(investor, address(invoiceNFT), InvoiceNFT.CreditScore.C);
-        uint256 tokenId = invoiceNFT.createInvoice(
-            creditor,
-            InvoiceNFT.Invoice(
-                "1234567890",
-                "Activity",
-                "USA",
-                100,
-                100,
-                100,
-                creditorCompany,
-                debtorCompany,
-                investorCompany,
-                InvoiceNFT.InvoiceStatus.NEW
-            )
-        );
+    function test_CreateInvoice_Success() public {
+        uint256 tokenId = invoiceNFT.createInvoice(creditor, invoice);
         assertEq(invoiceNFT.ownerOf(tokenId), creditor);
         assertEq(tokenId, 1);
         assertEq(invoiceNFT.balanceOf(creditor), 1);
-        assertEq(invoiceNFT.isApprovedForAll(creditor, owner), true);
     }
+
+    function test_CreateInvoice_Revert_NotOwner() public {
+        vm.prank(debtor);
+        vm.expectRevert(
+            abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, debtor)
+        );
+        invoiceNFT.createInvoice(creditor, invoice);
+    }
+
+    function test_CreateInvoice_MutipleInvoices() public {
+        InvoiceNFT.Invoice memory invoice2 = InvoiceNFT.Invoice(
+            "1111111111",
+            "Military",
+            "France",
+            block.timestamp,
+            3000,
+            900,
+            creditorCompany,
+            debtorCompany,
+            investorCompany,
+            InvoiceNFT.InvoiceStatus.NEW
+        );
+
+        uint256 tokenId1 = invoiceNFT.createInvoice(creditor, invoice);
+        uint256 tokenId2 = invoiceNFT.createInvoice(creditor, invoice2);
+        assertEq(invoiceNFT.ownerOf(tokenId1), creditor);
+        assertEq(invoiceNFT.ownerOf(tokenId2), creditor);
+        assertEq(tokenId1, 1);
+        assertEq(tokenId2, 2);
+    }
+
+    function test_CreateInvoice_CreateSameInvoiceTwice() public {
+        uint256 tokenId1 = invoiceNFT.createInvoice(creditor, invoice);
+        uint256 tokenId2 = invoiceNFT.createInvoice(debtor, invoice);
+        assertEq(invoiceNFT.ownerOf(tokenId1), creditor);
+        assertEq(invoiceNFT.ownerOf(tokenId2), debtor);
+        assertEq(tokenId1, 1);
+        assertEq(tokenId2, 2);
+    }
+}
+
+contract InvoiceNFTGetInvoiceTest is Test {
+    InvoiceNFT private invoiceNFT;
+    address private debtor = makeAddr("debtor");
+    address private creditor = makeAddr("creditor");
+    address private investor = makeAddr("investor");
+    address private owner = address(this);
+    InvoiceNFT.Company private creditorCompany;
+    InvoiceNFT.Company private debtorCompany;
+    InvoiceNFT.Company private investorCompany;
+    InvoiceNFT.Invoice private invoice;
+
+    function setUp() public {
+        invoiceNFT = new InvoiceNFT(owner);
+        investorCompany = InvoiceNFT.Company(
+            investor,
+            address(invoiceNFT),
+            InvoiceNFT.CreditScore.C
+        );
+        debtorCompany = InvoiceNFT.Company(
+            debtor,
+            address(invoiceNFT),
+            InvoiceNFT.CreditScore.B
+        );
+        creditorCompany = InvoiceNFT.Company(
+            creditor,
+            address(invoiceNFT),
+            InvoiceNFT.CreditScore.A
+        );
+        invoice = InvoiceNFT.Invoice(
+            "1234567890",
+            "Activity",
+            "USA",
+            block.timestamp,
+            1000,
+            300,
+            creditorCompany,
+            debtorCompany,
+            investorCompany,
+            InvoiceNFT.InvoiceStatus.NEW
+        );
+    }
+
+    function test_GetInvoice_Success() public {
+        uint256 tokenId = invoiceNFT.createInvoice(creditor, invoice);
+        InvoiceNFT.Invoice memory retrievedInvoice = invoiceNFT.getInvoice(tokenId);
+        assertEq(retrievedInvoice.id, invoice.id);
+    }
+    
+    function test_GetInvoice_Revert_NotOwner() public {
+        uint256 tokenId = invoiceNFT.createInvoice(creditor, invoice);
+        vm.prank(debtor);
+        vm.expectRevert(
+            abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, debtor)
+        );
+        invoiceNFT.getInvoice(tokenId);
+    }
+
+    function testFuzz_GetInvoice_Revert_NotMinted(uint256 tokenId) public {
+        vm.expectRevert(
+            abi.encodeWithSelector(IERC721Errors.ERC721NonexistentToken.selector, tokenId)
+        );
+        invoiceNFT.getInvoice(tokenId);
+    }
+
+    function testFuzz_GetInvoice_Revert_OneMinted(uint256 tokenId) public {
+        invoiceNFT.createInvoice(creditor, invoice);
+        if (tokenId != 1) {
+            vm.expectRevert(
+                abi.encodeWithSelector(IERC721Errors.ERC721NonexistentToken.selector, tokenId)
+            );
+            invoiceNFT.getInvoice(tokenId);
+        }
+    }
+    
 }
