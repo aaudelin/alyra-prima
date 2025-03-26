7bd74ad00481879d96dbd6dbabdfcb4e6f1d2444
Aurélien Audelin <aurelien.audelin@gmail.com>
2025-03-26 21:11:30 +0100

    feat: add collateral

--- backend/src/Collateral.sol
+++ backend/src/Collateral.sol
@@ -3,12 +3,20 @@ pragma solidity 0.8.29;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-abstract contract Collateral is Ownable {
+contract Collateral is Ownable {
     mapping(address => uint256) private collateral;
 
     constructor(address owner) Ownable(owner) {}
 
-    function deposit(address to, uint256 collateralAmount) external virtual;
+    function deposit(address to, uint256 collateralAmount) external onlyOwner {
+        collateral[to] += collateralAmount;
+    }
 
-    function withdraw(address from, uint256 collateralAmount) external virtual;
+    function withdraw(address from, uint256 collateralAmount) external onlyOwner {
+        collateral[from] -= collateralAmount;
+    }
+
+    function getCollateral(address account) external view onlyOwner returns (uint256) {
+        return collateral[account];
+    }
 }
---
+++ backend/test/Collateral.t.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Collateral} from "../src/Collateral.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CollateralTest is Test {
+    Collateral private collateral;
+    address private owner = address(this);
+    address private debtor = makeAddr("debtor");
+
+    function setUp() public {
+        collateral = new Collateral(owner);
+    }
+
+    function test_Deposit_Success() public {
+        collateral.deposit(debtor, 100);
+        assertEq(collateral.getCollateral(debtor), 100);
+    }
+
+    function test_Deposit_OnlyOwner() public {
+        vm.prank(debtor);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, debtor));
+        collateral.deposit(debtor, 100);
+    }
+
+    function test_Deposit_ZeroAmount() public {
+        collateral.deposit(debtor, 0);
+        assertEq(collateral.getCollateral(debtor), 0);
+    }
+
+    function test_Deposit_MultipleDeposits() public {
+        collateral.deposit(debtor, 100);
+        collateral.deposit(debtor, 200);
+        assertEq(collateral.getCollateral(debtor), 300);
+    }
+
+    function test_Withdraw_Success() public {
+        collateral.deposit(debtor, 100);
+        collateral.withdraw(debtor, 50);
+        assertEq(collateral.getCollateral(debtor), 50);
+    }
+
+    function test_Withdraw_OnlyOwner() public {
+        vm.prank(debtor);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, debtor));
+        collateral.withdraw(debtor, 50);
+    }
+
+    function test_Withdraw_NotEnoughCollateral() public {
+        vm.expectRevert();
+        collateral.withdraw(debtor, 150);
+    }
+
+    function test_Withdraw_ZeroAmount() public {
+        collateral.deposit(debtor, 100);
+        collateral.withdraw(debtor, 0);
+        assertEq(collateral.getCollateral(debtor), 100);
+    }
+
+    function test_Withdraw_MultipleWithdrawals() public {
+        collateral.deposit(debtor, 100);
+        collateral.withdraw(debtor, 50);
+        collateral.withdraw(debtor, 50);
+        assertEq(collateral.getCollateral(debtor), 0);
+    }
+
+    function test_Withdraw_Failure_MultipleWithdrawals() public {
+        collateral.deposit(debtor, 100);
+        collateral.withdraw(debtor, 50);
+        vm.expectRevert();
+        collateral.withdraw(debtor, 100);
+    }
+}
