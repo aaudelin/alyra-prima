f84253ae4b2854cba95ac8d5fc92e8cb429a1a2a
Aurélien Audelin <aurelien.audelin@gmail.com>
2025-04-03 17:49:46 +0200

    refactor: format forge

--- backend/script/PrimaTokenMintLocal.s.sol
+++ backend/script/PrimaTokenMintLocal.s.sol
@@ -15,7 +15,11 @@ contract PrimaTokenMintLocalScript is Script {
     function run(address primaTokenAddress) public {
         vm.startBroadcast(msg.sender);
 
-        address[3] memory tos = [0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 0x70997970C51812dc3A010C7d01b50e0d17dc79C8, 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC];
+        address[3] memory tos = [
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
+            0x70997970C51812dc3A010C7d01b50e0d17dc79C8,
+            0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC
+        ];
         uint256 amount = 10000 ether;
 
         primaToken = PrimaToken(primaTokenAddress);
