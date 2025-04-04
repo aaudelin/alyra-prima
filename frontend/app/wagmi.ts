51f85eb669f9dbcc126785b7a27b53e26cdcc62b
Aurélien Audelin <aurelien.audelin@gmail.com>
2025-04-04 23:39:45 +0200

    build: update env var

--- frontend/app/wagmi.ts
+++ frontend/app/wagmi.ts
@@ -1,9 +1,14 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { sepolia, foundry } from "wagmi/chains";
+import { http } from "wagmi";
 
 export const config = getDefaultConfig({
   appName: "Prima App",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID ?? "",
   chains: [foundry, sepolia],
   ssr: true,
+  transports: {
+    [sepolia.id]: http(process.env.NEXT_PUBLIC_RPC_URL),
+    [foundry.id]: http(),
+  },
 });
